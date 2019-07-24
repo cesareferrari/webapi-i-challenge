e026a45f66ddda6492a8a2e5698d99ccaef1ca99
Cesare Ferrari <cesare@maryandferrari.com>
2019-07-24 11:30:41 -0400

    Implement GET /api/users

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 // implement your API here
 const express = require('express');
 
+const db = require('./data/db.js');
+
 const server = express();
+
+server.get('/api/users', (req, res) => {
+  db.find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      res.status(500).json({success: false, err});
+    })
+});
+
+
+
 
 server.listen(4000, () => {
   console.log('Server listening on port 4000');
 });
+
