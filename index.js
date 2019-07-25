0a1b22176381261533a7e064c2a24731e9cec60d
Cesare Ferrari <cesare@maryandferrari.com>
2019-07-24 22:57:18 -0400

    Refactor PUT request

--- index.js
+++ index.js
@@ -72,17 +72,21 @@ server.put('/api/users/:id', (req, res) => {
   const user = req.body;
   console.log(user);
 
-  db.update(id, user)
-    .then(updated => {
-      if (updated) {
-        res.status(200).json({success: true, updated});
-      } else {
-        res.status(404).json({success: false, message: "I cannot find the user"})
-      }
-    })
-    .catch(err => {
-      res.status(500).json({success: false, err});
-    })
+  if (!user.name || !user.bio) {
+    res.status(400).json({errorMessage: "Please provide name and bio for the user."})
+  } else {
+    db.update(id, user)
+      .then(updated => {
+        if (updated) {
+          res.status(200).json(user);
+        } else {
+          res.status(404).json({message: "The user with the specified ID does not exist." })
+        }
+      })
+      .catch(err => {
+        res.status(500).json({error: "The user information could not be modified." });
+      })
+  }
 });
 
 
