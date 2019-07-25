4237b16a60f30f3a39d53fdcd98025bf11624634
Cesare Ferrari <cesare@maryandferrari.com>
2019-07-24 22:12:53 -0400

    Return 404 when user not found by ID

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ server.get('/api/users', (req, res) => {
       res.status(200).json(users);
     })
     .catch(err => {
-      res.status(500).json({success: false, err});
+      res.status(500).json({error: "The users information could not be retrieved."});
     })
 });
 
@@ -22,10 +22,14 @@ server.get('/api/users/:id', (req, res) => {
   const {id} = req.params;
   db.findById(id)
     .then(user => {
-      res.status(200).json(user);
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res.status(404).json({ message: "The user with the specified ID does not exist." });
+      }
     })
     .catch(err => {
-      res.status(500).json({success: false, err});
+      res.status(500).json({error: "The user information could not be retrieved."});
     })
 });
 
